0fbfd687f71d4b39d9c3fad8ceaeeac3097dcea5
michael <michael1mic1@gmail.com>
2024-03-18 21:49:25 +0200

    Added Welcome-section

--- AiLearner_Client/src/app/app.component.ts
+++ AiLearner_Client/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent }  from './components/header/header.component';
-
+import { WelcomeSectionComponent } from './components/welcome-section/welcome-section.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent, WelcomeSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

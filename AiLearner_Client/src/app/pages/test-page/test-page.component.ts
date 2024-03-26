f7368b705aa2e70ba97e46d98895d123b8909f8b
michael <michael1mic1@gmail.com>
2024-03-26 18:17:21 +0200

    Added rxjs state managment for user's id

--- AiLearner_Client/src/app/pages/test-page/test-page.component.ts
+++ AiLearner_Client/src/app/pages/test-page/test-page.component.ts
@@ -1,22 +1,48 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { UserDTO } from '../../models/UserDTO';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-test-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './test-page.component.html',
-  styleUrl: './test-page.component.css'
+  styleUrl: './test-page.component.css',
 })
 export class TestPageComponent {
+  userId: string | null = null;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {
+    this.userService.userId$.subscribe((id) => {
+      this.userId = id;
+      console.log('User ID updated:', id);
+    });
+  }
+
+  loginUser() {
+    const user: UserDTO = {
+      // Assuming your UserDTO has these fields; adjust accordingly
+      email: 'Michael1mic1@gmail.com',
+      password: 'abcd1234',
+    };
+
+    this.userService.loginUser(user).subscribe({
+      next: (response) => {
+        console.log('Logged in successfully');
+        // No need to manually set userId here, as it's handled by the subscription
+      },
+      error: (error) => {
+        console.error('Login error:', error);
+      },
+    });
+  }
 
   signUp() {
-    const email = "test@gmail.com";
-    const password = "password";
-    let userDto: UserDTO = {email:email,password:password}
+    const email = 'test@gmail.com';
+    const password = 'password';
+    let userDto: UserDTO = { email: email, password: password };
     // this.userService.signUp(userDto).subscribe((data) => {
     //   console.log(data);
     // });
--- AiLearner_Client/src/app/services/user.service.ts
+++ AiLearner_Client/src/app/services/user.service.ts
@@ -1,39 +1,68 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, tap, throwError } from 'rxjs';
 import { UserDTO } from '../models/UserDTO';
 
+/**
+ * Service for managing user-related operations.
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   baseUrl = 'https://localhost:7089/api/User/';
 
+  private userIdSource = new BehaviorSubject<string | null>(null);
+  userId$ = this.userIdSource.asObservable();
+
   constructor(private http: HttpClient) {}
 
+  /**
+   * Registers a new user.
+   * @param user - The user data to be registered.
+   * @returns An observable that emits the HTTP response.
+   */
   registerUser(user: UserDTO): Observable<any> {
-    return this.http.post(this.baseUrl + 'register', user, {
-      headers: new HttpHeaders({'Content-Type': 'application/json'}),
-      observe: 'response'
-    }).pipe(
-      catchError((error) => {
-        return throwError(() => error);
+    return this.http
+      .post(this.baseUrl + 'register', user, {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+        observe: 'response',
       })
-    );
+      .pipe(
+        catchError((error) => {
+          return throwError(() => error);
+        })
+      );
   }
 
+  /**
+   * Logs in a user.
+   * @param user - The user data to be logged in.
+   * @returns An observable that emits the HTTP response.
+   */
   loginUser(user: UserDTO): Observable<any> {
-    return this.http.post(this.baseUrl + 'login', user, {
-      headers: new HttpHeaders({'Content-Type': 'application/json'}),
-      observe: 'response',
-      withCredentials: true
-    }).pipe(
-      catchError((error) => {
-        return throwError(() => error);
+    return this.http
+      .post<{ userId: string }>(this.baseUrl + 'login', user, {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+        observe: 'response',
+        withCredentials: true,
       })
-    );
+      .pipe(
+        tap((response) => {
+          if (response.body?.userId) {
+            this.userIdSource.next(response.body.userId);
+          }
+        }),
+        catchError((error) => {
+          return throwError(() => error);
+        })
+      );
   }
 
+  /**
+   * Retrieves a list of users.
+   * @returns An observable that emits the list of users.
+   */
   getUsers(): Observable<string> {
     return this.http.get<string>('https://localhost:7089/test');
   }

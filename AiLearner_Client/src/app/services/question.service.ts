5232c2d153c7ec341cd7b1c4739a41eb1de42fe6
michael <michael1mic1@gmail.com>
2024-03-31 16:49:48 +0300

    Added qeustion service

---
+++ AiLearner_Client/src/app/services/question.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { environment } from '../../environments/environment.secret';
+import { QuestionDTO } from '../models/QuestionDTO';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+  secretUrl = environment.baseUrl;
+  baseUrl = this.secretUrl + '/question';
+
+  constructor(private http: HttpClient) { }
+
+  getQuestions(materialId: string): Observable<QuestionDTO[]> {
+    return this.http.get<QuestionDTO[]>(this.baseUrl + '/' + materialId);
+  }
+}

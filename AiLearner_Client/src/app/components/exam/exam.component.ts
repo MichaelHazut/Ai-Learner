6992f68366f66fdcb9fac00b79b38cdb6c9654e9
michael <michael1mic1@gmail.com>
2024-04-01 15:25:14 +0300

    Fixed indexing issue

--- AiLearner_Client/src/app/components/exam/exam.component.ts
+++ AiLearner_Client/src/app/components/exam/exam.component.ts
@@ -1,44 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { QuestionDTO } from '../../models/QuestionDTO';
 import { QuestionService } from '../../services/question.service';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { AnswerDTO } from '../../models/AnswerDTO';
+import { AnswerService } from '../../services/answer.service';
+import { QuestionAndAnswers } from '../../models/QuestionAndAnswers';
+import { QuestionComponent } from './question/question.component';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-exam',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, QuestionComponent],
   templateUrl: './exam.component.html',
-  styleUrl: './exam.component.css'
+  styleUrl: './exam.component.css',
 })
-export class ExamComponent {
+export class ExamComponent implements OnDestroy {
   materialId: string | null = null;
-  materialContent: string = '';
   questions: QuestionDTO[] = [];
+  answers: AnswerDTO[] = [];
+  questionsWithAnswers: QuestionAndAnswers[] = [];
+  currentQuestionIndex: number = 0;
+  routeSub!: Subscription;
 
   constructor(
     private questionsService: QuestionService,
+    private answersService: AnswerService,
     private route: ActivatedRoute,
     private router: Router
   ) {}
 
   ngOnInit() {
     this.materialId = this.route.snapshot.paramMap.get('id');
-    const navigation = this.router.getCurrentNavigation();
-    this.materialContent = navigation?.extras.state?.['materialContent'];
-    console.log(this.materialId);
+
     if (this.materialId) {
       this.loadQuestions();
+      this.loadAnswers();
     }
+    this.routeSub = this.route.paramMap.subscribe((params) => {
+      const index = params.get('questionIndex');
+      this.currentQuestionIndex = index ? parseInt(index, 10) : 0;
+    });
   }
 
   loadQuestions(): void {
     this.questionsService.getQuestions(this.materialId!).subscribe({
       next: (questions: QuestionDTO[]) => {
         this.questions = questions;
+        this.combineQuestionsAndAnswers();
       },
       error: (error) => {
         console.error('There was an error fetching questions!', error);
       },
     });
+  }
+  loadAnswers(): void {
+    this.answersService.getAnswers(this.materialId!).subscribe({
+      next: (answers: AnswerDTO[]) => {
+        this.answers = answers;
+        this.combineQuestionsAndAnswers();
+      },
+      error: (error) => {
+        console.error('There was an error fetching questions!', error);
+      },
+    });
+  }
+  combineQuestionsAndAnswers(): void {
+    if (this.questions.length && this.answers.length) {
+      this.questionsWithAnswers = this.questions.map((question) => ({
+        question,
+        answers: this.answers.filter(
+          (answer) => answer.questionId === question.id
+        ),
+      }));
+    }
+  }
+  handleAnswerSelection(anserId: number): void {
+    this.currentQuestionIndex++;
+
+    if (this.currentQuestionIndex <= this.questionsWithAnswers.length) {
+      this.router.navigate([
+        `/study-hub/materials/${this.materialId}/exam/${this.currentQuestionIndex}`,
+      ]);
+    } else {
+      this.router.navigate([`/study-hub/materials/${this.materialId}/result`]);
+    }
+  }
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
   }
 }
--- AiLearner_Client/src/app/components/exam/question/question.component.ts
+++ AiLearner_Client/src/app/components/exam/question/question.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output, input } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, Output, input } from '@angular/core';
 import { common } from '@mui/material/colors';
 import { QuestionDTO } from '../../../models/QuestionDTO';
 import { AnswerDTO } from '../../../models/AnswerDTO';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-question',
@@ -11,16 +13,27 @@ import { AnswerDTO } from '../../../models/AnswerDTO';
   templateUrl: './question.component.html',
   styleUrl: './question.component.css',
 })
-export class QuestionComponent {
+export class QuestionComponent implements OnDestroy{
   @Input() question!: QuestionDTO;
   @Input() answers!: AnswerDTO[];
-
   @Output() answerSelected = new EventEmitter<number>();
+  
+  routeSub!: Subscription;
+  questionNumber: string | null = '';
+
+  constructor(private route: ActivatedRoute) {}
   ngOnInit() {
-    console.log('Question:', this.question);
-    console.log('Answers:', this.answers);
+    this.questionNumber = this.route.snapshot.paramMap.get('questionIndex') || '';
+    this.routeSub = this.route.paramMap.subscribe(params => {
+      this.questionNumber = params.get('questionIndex');
+      
+    });
   }
   selectAnswer(answerId: number) {
     this.answerSelected.emit(answerId);
+  }
+
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
   }
 }

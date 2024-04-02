a56fd767f6618c7b66d78f28bc65c9de6772aaf2
michael <michael1mic1@gmail.com>
2024-04-02 21:43:06 +0300

    Added exam data global property

---
+++ AiLearner_Client/src/app/components/exam/exam-result/exam-result.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exam-result',
+  standalone: true,
+  imports: [],
+  templateUrl: './exam-result.component.html',
+  styleUrl: './exam-result.component.css'
+})
+export class ExamResultComponent {
+
+}
--- AiLearner_Client/src/app/components/exam/exam.component.ts
+++ AiLearner_Client/src/app/components/exam/exam.component.ts
@@ -8,6 +8,8 @@ import { AnswerService } from '../../services/answer.service';
 import { QuestionAndAnswers } from '../../models/QuestionAndAnswers';
 import { QuestionComponent } from './question/question.component';
 import { Subscription } from 'rxjs';
+import { ExamDataService } from '../../services/exam-data.service';
+import { Exam } from '../../models/Exam';
 @Component({
   selector: 'app-exam',
   standalone: true,
@@ -21,64 +23,80 @@ export class ExamComponent implements OnDestroy {
   answers: AnswerDTO[] = [];
   questionsWithAnswers: QuestionAndAnswers[] = [];
   currentQuestionIndex: number = 0;
-  routeSub!: Subscription;
+  private subscriptions: Subscription[] = [];
+  examData: Exam | null = null;
 
   constructor(
     private questionsService: QuestionService,
     private answersService: AnswerService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private examDataService: ExamDataService
   ) {}
 
   ngOnInit() {
     this.materialId = this.route.snapshot.paramMap.get('id');
 
-    if (this.materialId) {
-      this.loadQuestions();
-      this.loadAnswers();
-    }
-    this.routeSub = this.route.paramMap.subscribe((params) => {
-      const index = params.get('questionIndex');
-      this.currentQuestionIndex = index ? parseInt(index, 10) : 0;
-    });
+    // if (this.materialId) {
+    //   this.loadQuestions();
+    //   this.loadAnswers();
+    // }
+    this.subscriptions.push(
+      this.examDataService.examData$.subscribe((examData) => {
+        this.examData = examData;
+        console.log('examData: ', examData);
+        console.log("length: " , examData?.questions.length);
+        console.log("currentQuestionIndex: ", this.currentQuestionIndex);
+      })
+    );
+    this.subscriptions.push(
+      this.route.paramMap.subscribe((params) => {
+        const index = params.get('questionIndex');
+        this.currentQuestionIndex = index ? parseInt(index, 10) : 0;
+      })
+    );
+    this.examDataService.getExamData(parseInt(this.materialId!));
   }
 
-  loadQuestions(): void {
-    this.questionsService.getQuestions(this.materialId!).subscribe({
-      next: (questions: QuestionDTO[]) => {
-        this.questions = questions;
-        this.combineQuestionsAndAnswers();
-      },
-      error: (error) => {
-        console.error('There was an error fetching questions!', error);
-      },
-    });
-  }
-  loadAnswers(): void {
-    this.answersService.getAnswers(this.materialId!).subscribe({
-      next: (answers: AnswerDTO[]) => {
-        this.answers = answers;
-        this.combineQuestionsAndAnswers();
-      },
-      error: (error) => {
-        console.error('There was an error fetching questions!', error);
-      },
-    });
-  }
-  combineQuestionsAndAnswers(): void {
-    if (this.questions.length && this.answers.length) {
-      this.questionsWithAnswers = this.questions.map((question) => ({
-        question,
-        answers: this.answers.filter(
-          (answer) => answer.questionId === question.id
-        ),
-      }));
-    }
-  }
+  // loadQuestions(): void {
+  //   this.questionsService.getQuestions(this.materialId!).subscribe({
+  //     next: (questions: QuestionDTO[]) => {
+  //       this.questions = questions;
+  //       this.combineQuestionsAndAnswers();
+  //     },
+  //     error: (error) => {
+  //       console.error('There was an error fetching questions!', error);
+  //     },
+  //   });
+  // }
+  // loadAnswers(): void {
+  //   this.answersService.getAnswers(this.materialId!).subscribe({
+  //     next: (answers: AnswerDTO[]) => {
+  //       this.answers = answers;
+  //       this.combineQuestionsAndAnswers();
+  //     },
+  //     error: (error) => {
+  //       console.error('There was an error fetching questions!', error);
+  //     },
+  //   });
+  // }
+  // combineQuestionsAndAnswers(): void {
+  //   if (this.questions.length && this.answers.length) {
+  //     this.questionsWithAnswers = this.questions.map((question) => ({
+  //       question,
+  //       answers: this.answers.filter(
+  //         (answer) => answer.questionId === question.id
+  //       ),
+  //     }));
+  //   }
+  // }
   handleAnswerSelection(anserId: number): void {
     this.currentQuestionIndex++;
 
-    if (this.currentQuestionIndex <= this.questionsWithAnswers.length) {
+    if (
+      this.examData &&
+      this.currentQuestionIndex <= this.examData.questions.length
+    ) {
       this.router.navigate([
         `/study-hub/materials/${this.materialId}/exam/${this.currentQuestionIndex}`,
       ]);
@@ -87,6 +105,6 @@ export class ExamComponent implements OnDestroy {
     }
   }
   ngOnDestroy() {
-    this.routeSub.unsubscribe();
+    this.subscriptions.forEach((subscriptions) => subscriptions.unsubscribe());
   }
 }

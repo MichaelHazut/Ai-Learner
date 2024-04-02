b8c9bd6ba29e6bb19b110f19d2dd78ff7bc71e7e
michael <michael1mic1@gmail.com>
2024-04-02 21:42:13 +0300

    Added global state manager for exam data

--- AiLearner_Client/src/app/services/exam-data.service.ts
+++ AiLearner_Client/src/app/services/exam-data.service.ts
@@ -3,16 +3,20 @@ import { MaterialService } from './material.service';
 import { QuestionService } from './question.service';
 import { AnswerService } from './answer.service';
 import { UserAnswersService } from './user-answers.service';
-import { Observable, forkJoin, switchMap } from 'rxjs';
+import { BehaviorSubject, Observable, forkJoin, switchMap } from 'rxjs';
 import { MaterialDTO } from '../models/MaterialDTO';
 import { QuestionDTO } from '../models/QuestionDTO';
 import { AnswerDTO } from '../models/AnswerDTO';
 import { UserAnswersDTO } from '../models/UserAnswersDTO';
+import { Exam } from '../models/Exam';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ExamDataService {
+  private examData = new BehaviorSubject<Exam | null>(null);
+  examData$ = this.examData.asObservable();
+
   constructor(
     private materialService: MaterialService,
     private questionService: QuestionService,
@@ -20,25 +24,30 @@ export class ExamDataService {
     private userAnswersService: UserAnswersService
   ) {}
 
-  fetchExamData(materialId: number): Observable<{ material: MaterialDTO; questions: QuestionDTO[]; answers: AnswerDTO[]; userAnswers: UserAnswersDTO[]; }> {
+  fetchExamData(
+    materialId: number
+  ): Observable<{
+    material: MaterialDTO;
+    questions: QuestionDTO[];
+    answers: AnswerDTO[];
+    userAnswers: UserAnswersDTO[];
+  }> {
     return forkJoin({
       material: this.materialService.getMaterialById(materialId),
       questions: this.questionService.getQuestions(materialId),
       answers: this.answerService.getAnswers(materialId),
-      userAnswers: this.userAnswersService.getUserAnswers(materialId)
+      userAnswers: this.userAnswersService.getUserAnswers(materialId),
     });
   }
 
-  logData(materialId: number): void {
+  getExamData(materialId: number): void {
     this.fetchExamData(materialId).subscribe({
       next: ({ material, questions, answers, userAnswers }) => {
-        console.log('Material:', material);
-        console.log('Questions:', questions);
-        console.log('Answers:', answers);
-        console.log('User Answers:', userAnswers);
+        let examObj = new Exam(material, questions, answers, userAnswers);
+        this.examData.next(examObj);
       },
       error: (error) => console.error('Error fetching data:', error),
-      complete: () => console.log('Data fetching complete.')
+      complete: () => console.log('Data fetching complete.'),
     });
   }
 }

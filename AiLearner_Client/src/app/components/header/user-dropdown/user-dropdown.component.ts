a9c428cc86d3790ca6fafb898a2ef1b9bfc104d7
michael <michael1mic1@gmail.com>
2024-04-30 10:27:19 +0300

    Added close on click outside user dropdown

--- AiLearner_Client/src/app/components/header/user-dropdown/user-dropdown.component.ts
+++ AiLearner_Client/src/app/components/header/user-dropdown/user-dropdown.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 import { UserService } from '../../../services/user.service';
 import { Route, Router } from '@angular/router';
 
@@ -14,7 +14,11 @@ export class UserDropdownComponent {
   userEmail: string = 'email not found';
   dropdownOpen = false;
 
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private eRef: ElementRef
+  ) {}
 
   ngOnInit() {
     this.userService.getIsAuthenticated().subscribe({
@@ -41,6 +45,15 @@ export class UserDropdownComponent {
   toggleDropdown(): void {
     this.dropdownOpen = !this.dropdownOpen;
   }
+
+  //Close dropdown when clicked outside
+  @HostListener('document:click', ['$event'])
+  clickout(event: Event): void {
+    if (!this.eRef.nativeElement.contains(event.target as Node)) {
+      this.dropdownOpen = false;
+    }
+  }
+
   logout() {
     this.router.navigate(['/signout']);
   }

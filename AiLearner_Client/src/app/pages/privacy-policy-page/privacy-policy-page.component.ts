1b7aa9cb5d365c34a347dfbc6094c234383b7555
michael <michael1mic1@gmail.com>
2024-04-30 12:19:31 +0300

    Added privacy policy

---
+++ AiLearner_Client/src/app/pages/privacy-policy-page/privacy-policy-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-policy-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './privacy-policy-page.component.html',
+  styleUrl: './privacy-policy-page.component.css'
+})
+export class PrivacyPolicyPageComponent {
+
+}

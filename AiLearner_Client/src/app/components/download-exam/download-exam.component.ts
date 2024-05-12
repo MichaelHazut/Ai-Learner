40da8be83a066c6f8e9ff4c1eccbfb0503fd440c
michael <michael1mic1@gmail.com>
2024-05-12 18:28:35 +0300

    Changed download header

--- AiLearner_Client/src/app/components/download-exam/download-exam.component.ts
+++ AiLearner_Client/src/app/components/download-exam/download-exam.component.ts
@@ -15,9 +15,12 @@ export class DownloadExamComponent {
   downloadPdf() {
     const materialId = this.route.snapshot.paramMap.get('id');
     this.pdfService.getPDF(parseInt(materialId!)).subscribe((response) => {
-      let fileName = response.headers.get('content-disposition')?.split(';')[1].split('=')[1];
+      let fileName = response.headers.get('content-disposition')?.split(';')[1].split('=')[1].slice(0, -1).slice(1);
+      console.log(response.headers.get('content-disposition'));
+      console.log(response.headers.get('content-disposition')?.split(';')[1].split('=')[1]);
       let blob: Blob = response.body as Blob;
       let a = document.createElement('a');
+      console.log(fileName);
       a.download = fileName!;
       a.href = window.URL.createObjectURL(blob);
       a.click();
